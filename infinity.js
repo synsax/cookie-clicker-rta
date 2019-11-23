b10b047cc815e38ebcd98c97fc9fa66aed199cff
synsax <www.vantanphsh1036@gmail.com>
2019-11-23 23:27:41 +0900

    corrected the path to big dream

--- infinity.js
+++ infinity.js
@@ -3,10 +3,18 @@ const puppeteer = require('puppeteer');
 const url = 'https://orteil.dashnet.org/cookieclicker/';
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--window-size=960,720'],
+  });
   const page = await browser.newPage();
+  await page.setViewport({width: 960, height: 720});
+  let i = 0;
+  page.on('requestfinished', async req => {
+    if (++i === 60) {
+      await page.evaluate(() => {Game.cookies=Infinity;return;});
+    }
+  });
   await page.goto(url, {waitUntil: ['load', 'networkidle2']});
-  await page.evaluate(() => {Game.cookies=Infinity;return;});
-  await page.screenshot({path: 'my_infinity_dream.png'});
 })();
 
